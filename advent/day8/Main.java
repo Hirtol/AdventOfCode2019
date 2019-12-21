58be5362a61f922e79e8e8a7aca38b861d21f766
Valentijn Hol <valentijnhol@hotmail.nl>
2019-12-21 19:22:55 +0100

    Refactoring day 8

--- advent/day8/Main.java
+++ advent/day8/Main.java
@@ -9,13 +9,10 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Main {
-
 	public static final int WIDTH = 25;
 	public static final int HEIGHT = 6;
 	public static final int SIZE = WIDTH * HEIGHT;
-	public static List<String> layers = new ArrayList<>();
 	public static List<List<Integer>> twoLayers = new ArrayList<>();
-	public static List<Integer> pixelList;
 
 	private static <E> long count(List<E> list, E element){
 		return list.stream().filter(p -> p == element).count();
@@ -30,7 +27,7 @@ public class Main {
 	}
 
 	public static void part2(){
-		pixelList = IntStream.range(0, SIZE).map(number -> twoLayers.stream().filter(list -> list.get(number) != 2).findFirst().map(list -> list.get(number)).get()).boxed().collect(Collectors.toList());
+		List<Integer> pixelList = IntStream.range(0, SIZE).map(number -> twoLayers.stream().filter(list -> list.get(number) != 2).findFirst().map(list -> list.get(number)).get()).boxed().collect(Collectors.toList());
 		for (int row = 0; row < HEIGHT; row++) {
 			for (int column = 0; column < WIDTH; column++) {
 				if(pixelList.get(row*WIDTH + column) == 1)
@@ -45,19 +42,14 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader read = new BufferedReader(new FileReader("C:\\Users\\Valentijn\\Desktop\\Git-Projects\\CodeAdvent2019\\src\\advent\\day8\\input.txt"));
 		String input = read.readLine();
-
-		for (int i = 0; input.length() > SIZE -1+ SIZE *i; i++) {
-			layers.add(input.substring(i* SIZE, SIZE + SIZE *i));
-		}
-
+		List<String> layers = new ArrayList<>();
+		for (int i = 0; input.length() > SIZE-1+SIZE*i; i++)
+			layers.add(input.substring(i* SIZE, SIZE+SIZE*i));
 		// Should I have used a for loop? Yes. Will I? No.
 		twoLayers = layers.stream().map(String::toCharArray).
 				map(ar -> IntStream.range(0, ar.length).map(p -> Character.getNumericValue(ar[p]))
 						.boxed().collect(Collectors.toList()))
 				.collect(Collectors.toList());
-
-		part1();
-		
+		part2();
 	}
-
 }
